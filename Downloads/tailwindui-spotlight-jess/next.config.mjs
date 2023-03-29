89dba1bec0d4854a5b3c93c13def285362d16d12
Jessica Donig <jessdonig@gmail.com>
2023-03-28 19:51:26 -0700

    started working on portfolio site

---
+++ Downloads/tailwindui-spotlight-jess/next.config.mjs
@@ -0,0 +1,33 @@
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+import rehypePrism from '@mapbox/rehype-prism'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+  },
+}
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+})
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.v5.airtableusercontent.com',
+      },
+    ],
+  },
+}
+
+export default withMDX(nextConfig)
